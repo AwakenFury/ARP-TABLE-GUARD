917c77db61d3e1f0c8c99e4aed721adf0a599e9d
Awaken_Fury <josesantana1528@gmail.com>
2026-07-09 13:03:40 -0400

    Add files via upload

---
+++ Clear Route.bat
@@ -0,0 +1,44 @@
+@Echo Cleaning Route
+route print
+
+route delete ff00::/8
+route delete ::1/128
+route delete ::1/128                  
+route delete fe80::/64                
+route delete fe80::d4d5:37b:6503:5972/128                                
+route delete ff00::/8                 
+route delete ff00::/8   
+route delete fe80::39df:7e94:f560:38ca/128              
+  
+route delete 72.69.144.0    
+route delete 72.69.144.9  
+
+
+route delete 96.246.55.0    
+route delete 96.246.55.166       
+route delete 96.246.55.255      
+route delete 127.0.0.1       
+
+route delete 72.69.144.221  
+route delete 72.69.144.255  
+route delete 192.168.0.0  
+route delete 127.0.0.0
+route delete 127.0.0.1
+route delete 192.168.0.100  
+route delete 192.168.0.255  
+route delete 100.37.82.255
+route delete 100.37.82.67
+route delete 100.37.82.0    
+route delete 100.37.82.67 
+route delete 100.37.82.255  
+route delete  100.37.196.0    
+route delete 100.37.196.83  
+route delete 100.37.196.255  
+route delete 127.255.255.255  
+route delete 224.0.0.0       
+route delete 224.0.0.0       
+route delete 255.255.255.255  
+route delete 255.255.255.255
+
+route print 
+@pause
