a1f2c4913263f1aa2ebcb7a1876a0d20b6c2da76
Awaken_Fury <josesantana1528@gmail.com>
2026-07-09 13:07:04 -0400

    Add files via upload

---
+++ SearchUI.bat
@@ -0,0 +1,10 @@
+:start
+
+
+
+taskkill /f /im SearchUI.exe
+taskkill /pid 2808
+%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy %windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy.old
+
+
+goto start
